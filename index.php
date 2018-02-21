459d6ece739e5e0e45638992135cec56fdbb04bc
Raquel Zempoalteca <rzempoalteca@csumb.edu>
2018-02-21 00:30:47 +0000

    Changes

--- index.php
+++ index.php
@@ -30,17 +30,54 @@
             $allPlayers = array (
                 $player1,
                 $player2,
-                $player3
+                $player3,
+                $player4
                 );
             
             function printGameState($allPlayers) {
                 foreach ($allPlayers as $player) {
                     echo "<img src ='" . $player['imgURL'] . "' />";
-                    echo $player['name'] . "'/>";
+                    echo $player['name'] . "br/>";
                 }
             }
             
             printGameState($allPlayers);
+            
+            getImgURLForCardIndex(0);
+            
+            //pseudocode
+            // create and array of 52 cards
+                // each card on associative array => suit, rank, imgURL, points
+            // shuffle the array
+            // pop off one card at a time to generate the hand
+
+            function getImgURLForCardIndex($index) {
+                //get a num from 0 to 51
+                // return an img url
+                
+                $suitIndex = floor ($index / 13);
+                
+                echo "suitIndex: $suitIndex";
+            }
+
+            function generateDeck() {
+                $suits = array ("clubs", "spades", "hearts", "diamonds");
+                
+                $deck = array();
+                
+                for ($i = 0; $i <= 3; $i++) {
+                    for($j = 1; $j <= 13; $j++) {
+                        
+                        
+                    }
+                    
+                }
+                // for ($i = 0; $i < 51; $i++) {
+                //     $card = array(
+                //         'imgURL' => ""
+                //         );
+                // }
+            }
 
         ?>
     </body>
