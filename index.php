4415ed2bdebd2ec9b4ff3cb4b1305966bdebfd80
Raquel Zempoalteca <rzempoalteca@csumb.edu>
2018-03-04 20:15:36 +0000

    folder/code cleanup & few basic comments

--- index.php
+++ index.php
@@ -10,13 +10,15 @@
         <header>
             <h1 style="font-size:60px;">Silver Jack</h1>
         </header>
+
         <audio autoplay>
                 <source src=sound/happyKids.mp3 type=audio/mpeg>
                 Your browser does not support the audio element.
         </audio>
+        
         <?php
-            
-            // initializa a list of players
+        
+            // initializing a list of players
             $player1 = array(
                 'name' => 'Irais',
                 'imgURL' => './img/user_pics/me.png',
@@ -25,7 +27,7 @@
                 );
             $player2 = array(
                 'name' => 'Raquel',
-                'imgURL' => './img/user_pics/Raquels_pic.jpg',
+                'imgURL' => './img/user_pics/Raquel.jpg',
                 'hand' => array(),
                 'points' => 0
                 );
@@ -41,7 +43,6 @@
                 'hand' => array(),
                 'points' => 0
                 );
-
             
             $allPlayers = array (
                 $player1,
@@ -50,8 +51,7 @@
                 $player4
                 );
             
-            
-            
+            // displays the names, pictures, cards/suits in deck of the players
             function printGameState($allPlayers) {
                 foreach ($allPlayers as $player) {
                     echo "<img src ='" . $player['imgURL'] . "' width = 150px />";
@@ -59,10 +59,10 @@
                     for ($j = 0; $j < sizeof($player['hand']); $j++) {
                         displayCard($player['hand'][$j][0], $player['hand'][$j][1]);
                     }
-                    //echo "<br/>";
                 }
             }
             
+            // creates the deck of cards
             function generateDeck()
             {
                 $cardArray = array();
@@ -75,6 +75,7 @@
                 return $cardArray;
             }
             
+            // suits of the deck
             function displayCard ($symbol, $value) {
                     switch ($symbol) {
                         case 0: echo "<img class = 'card' src='img/cards/clubs/$value.png' id = clubs".$value." alt='clubs".$value."' title= 'card' width = 100>";
@@ -86,8 +87,8 @@
                         case 3: echo "<img class = 'card' src='img/cards/spades/$value.png' id = spades".$value ." alt='spades".$value."' title= 'card' width = 100>";
                                 break;
                     }
-                }
-                
+            }
+            
             function pickCard (&$player, &$cardArray) {
                 $symbol = rand (0,3);
                 $index = array_rand ($cardArray[$symbol], 1);
@@ -100,19 +101,17 @@
                 array_push($player['hand'],array($symbol,$index+1));
                 $player['points'] += $index+1;
             }
-                
+            
             function displayHand($player) {
                 for ($j = 0; $j < sizeof($player['hand']); $j++) {
                     displayCard($player['hand'][$j][0], $player['hand'][$j][1]);
                 }
             }
             
+            // picks a player depending on the points and array size
+            // then displays the winner(s) along with the points they earned
             function displayWinner($allPlayers){
                 echo "<h3>";
-                // echo "<audio autoplay>
-                //     <source src=sound/Win.mp3 type=audio/mpeg>
-                //     Your browser does not support the audio element.
-                //     </audio>";
                 $winner = array('points' => 0);
                 $totalPoints = 0;
                 $tmp = array();
@@ -140,13 +139,13 @@
                 }
                     echo "!!</i></h3>";
             }
-            
+
             function displayPoints(&$allPlayers){
                 foreach($allPlayers as $player) {
                     echo "<div class='point'>".$player['points']."</div>";
                 }
             }
-            
+    
             function play($limit, &$allPlayers) {
                 $cardArray = generateDeck();
                 foreach($allPlayers as &$player) {
@@ -157,10 +156,8 @@
                     displayHand($player);
                 }              
             }
-            
-            
-                  
-            // making the user's pictures display in a random order
+
+            // making the pictures and names of players display randomly
             shuffle ($allPlayers);      
               
             echo "<div class='players'>";
@@ -180,8 +177,8 @@
         
         <div id = "main" style = "text-align:center">
             <form>
-                <input type = "submit" value = "Play again!" style = "font-size: 25px"/>
-            </for>
+                <input type = "submit" value = "Play again!" style = "font-size: 35px"/>
+            </form>
         </div>
         
     </body>
